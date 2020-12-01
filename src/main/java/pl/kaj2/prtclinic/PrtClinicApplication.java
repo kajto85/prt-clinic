db489db75daccedddb188facba7d1453649dd7b6
sitar <sitarskim@gmail.com>
2020-12-01 22:18:39 +0100

    project start

---
+++ src/main/java/pl/kaj2/prtclinic/PrtClinicApplication.java
@@ -0,0 +1,13 @@
+package pl.kaj2.prtclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrtClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrtClinicApplication.class, args);
+    }
+
+}
